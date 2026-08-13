d118f6c5b5f2a43c9e27966430935ea9ce049419
darkironman-collab <darkironman@kpgs.xyz>
2026-08-13 23:47:21 +0530

    Require GeckoView-supported Android version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
 
     defaultConfig {
         applicationId = "com.extremeos.vpnonlybrowser"
-        minSdk = 23
+        minSdk = 26
         targetSdk = 36
         versionCode = 1
         versionName = "1.0.0"
